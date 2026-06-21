71dacf3d90369d858cdcbd1ed327ac4a4f0dd969
2021413 <hantonini222@gmail.com>
2026-06-21 21:40:05 +0200

    feat(web): tableau de bord avec compteurs du parc

---
+++ src/Locatic.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Locatic.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Locatic.Web.Models;
+
+namespace Locatic.Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ITableauDeBordService _tableauDeBord;
+
+    public HomeController(ITableauDeBordService tableauDeBord)
+    {
+        _tableauDeBord = tableauDeBord;
+    }
+
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var aujourdhui = DateOnly.FromDateTime(DateTime.Today);
+        var stats = await _tableauDeBord.ObtenirStatsAsync(aujourdhui, cancellationToken);
+        return View(stats);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
